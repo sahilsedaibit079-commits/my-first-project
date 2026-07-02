f5a8d4baf3920b36e406d8ab90908bf78b919f24
sahilsedaibit079-commits <sahilsedai.bit079@mbman.edu.np>
2026-07-02 23:42:25 +0545

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# hello.py
+name = input("What's your name? ")
+print(f"Hello, {name}! Welcome to your first GitHub push.")
+
+# A tiny bonus: basic calculator
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(f"Sum: {num1 + num2}")
+print(f"Product: {num1 * num2}")
